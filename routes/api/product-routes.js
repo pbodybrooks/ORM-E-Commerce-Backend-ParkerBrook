9ae2b9f3c98eaaffd20513789afce645551ede39
Parker <parkerlamarbrook@gmail.com>
2023-05-22 14:57:40 -0600

    category model filled in

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,11 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [Category, Tag]
+  })
+    .then((products) => res.json(products))
+    .catch((err) => res.status(500).json(err));
 });
 
 // get one product
